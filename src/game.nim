c22ec620254e04cf986d26cf6eae680d24591325
Eray Zesen <erayzesen@gmail.com>
2025-12-21 10:30:45 +0300

    Release update

--- src/game.nim
+++ src/game.nim
@@ -141,10 +141,11 @@ proc drawGameOver() =
 #endregion
 
 #region Sounds 
-var flapSound: Sound
-var scoreSound: Sound
-var hitSound: Sound
-var dieSound: Sound
+
+var flapSound = newSound("src/resources/sounds/flap.mp3",SoundType.Static)
+var scoreSound = newSound("src/resources/sounds/score.mp3",SoundType.Static)
+var hitSound = newSound("src/resources/sounds/hit.mp3",SoundType.Static)
+var dieSound = newSound("src/resources/sounds/die.mp3",SoundType.Static)
 #endregion
 
 #region Game
@@ -188,10 +189,6 @@ proc restartGame() =
 
 proc load() =
   # Resource loading
-  flapSound = newSound("src/resources/sounds/flap.mp3",SoundType.Static)
-  scoreSound = newSound("src/resources/sounds/score.mp3",SoundType.Static)
-  hitSound = newSound("src/resources/sounds/hit.mp3",SoundType.Static)
-  dieSound = newSound("src/resources/sounds/die.mp3",SoundType.Static)
   
   logoTexture = newTexture("src/resources/game_logo.png")
   startInfoTexture= newTexture("src/resources/start_info.png")
